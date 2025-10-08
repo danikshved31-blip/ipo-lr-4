b8a734f5b93dc0c3d80c2daef0b35bc045216c05
danchik <danikshved31@gmail.com>
2025-10-08 17:00:32 +0300

    izi forpopizi

--- task_3/main.py
+++ task_3/main.py
@@ -1,9 +1,15 @@
 num = int(input("Введите число: "))
 if num < 1:
     print("Число не простое")
+elif num == 1 and num == 2:
+    print("число простое")
 else:
     i = 2
     while i < num:
-        num % i != 0
-        i+ = 1
-        print ("Простое чило")
+        if num % i != 0:   
+            i+= 1
+            print ("число простое ")
+            break
+        else:
+            print("число не простое")
+            break
